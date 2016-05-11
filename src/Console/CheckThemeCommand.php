94236d2b41b772e79fb94c1f4d7e3eb03e8327c6
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-05-10 23:18:38 -0700

    Move and cleanup check:theme command.

---
+++ src/Console/CheckThemeCommand.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SSNepenthe\Soter\Console;
+
+use SSNepenthe\Soter\Config;
+use SSNepenthe\Soter\Container;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class CheckThemeCommand extends Command {
+	public function configure() {
+		$this->setName( 'check:theme' )
+			->setDescription(
+				'Check a single theme for security vulnerabilities.'
+			)
+			->addArgument(
+				'slug',
+				InputArgument::REQUIRED,
+				'Theme slug.'
+			)
+			->addArgument(
+				'version',
+				InputArgument::OPTIONAL,
+				'Theme version.'
+			);
+	}
+
+	public function execute( InputInterface $input, OutputInterface $output ) {
+		$io = new SymfonyStyle( $input, $output );
+		$slug = $input->getArgument( 'slug' );
+		$version = $input->getArgument( 'version' );
+		$client = Container::get( 'client' );
+
+		if ( in_array( $slug, Config::get( 'package.ignored' ) ) ) {
+			$continue = $io->choice(
+				'This package has been flagged to be ignored in your config.' .
+				' Would you like to check it anyway?',
+				[ 'no', 'yes' ]
+			);
+
+			if ( 'no' === $continue ) {
+				die(1);
+			}
+		}
+
+		$response = $client->check_theme( $slug );
+
+		if ( $response->is_error() ) {
+			$io->error( sprintf( 'HTTP Error %s' ), $response->status_code() );
+
+			die(1);
+		}
+
+		$advisories = $response->advisories_by_version( $version );
+
+		if ( empty( $advisories ) ) {
+			$io->success( 'Theme has no known vulnerabilites.' );
+		} else {
+			$count = count( $advisories );
+
+			$io->warning( sprintf(
+				'%s known %s.',
+				$count, 1 === $count ? 'vulnerability' : 'vulnerabilities'
+			) );
+
+			$io->block( $advisories );
+		}
+	}
+}
