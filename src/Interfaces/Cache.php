067af0595fc204e86d84626f769997e1a273eef2
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-03 10:33:19 -0700

    Add cache interface.

---
+++ src/Interfaces/Cache.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Cache provider interface.
+ *
+ * @package soter
+ */
+
+namespace SSNepenthe\Soter\Interfaces;
+
+/**
+ * Simple cache provider interface.
+ */
+interface Cache {
+	/**
+	 * Determine whether a given key exists in the cache.
+	 *
+	 * @param  string $id Cache key.
+	 *
+	 * @return bool
+	 */
+	public function contains( $id );
+
+	/**
+	 * Retrieve a given key from the cache.
+	 *
+	 * @param  string $id Cache key.
+	 *
+	 * @return mixed
+	 */
+	public function fetch( $id );
+
+	/**
+	 * Save data to the cache.
+	 *
+	 * @param  string $id       Cache key.
+	 * @param  mixed  $data     Data to save.
+	 * @param  int    $lifetime Cache entry lifetime.
+	 *
+	 * @return bool
+	 */
+	public function save( $id, $data, $lifetime );
+}
