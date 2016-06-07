8fab5147ce38617088a5947d46e69d5194a6588c
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-06-06 22:51:53 -0700

    Update robo.

--- RoboFile.php
+++ RoboFile.php
@@ -2,14 +2,14 @@
 
 class RoboFile extends \Robo\Tasks {
     /**
-	 * Update package version.
+	 * Update plugin version.
 	 *
 	 * @param string $version The new plugin version.
 	 */
 	public function versionBump( $version ) {
-		$this->taskReplaceInFile( sprintf( '%s/src/Config.php', __DIR__ ) )
-			->regex( '/const VERSION.*$/m' )
-			->to( sprintf( 'const VERSION = \'%s\';', $version ) )
+		$this->taskReplaceInFile( sprintf( '%s/soter.php', __DIR__ ) )
+			->regex( '/Version:.*$/m' )
+			->to( sprintf( 'Version: %s', $version ) )
 			->run();
 	}
 }
