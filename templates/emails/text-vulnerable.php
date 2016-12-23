242925c6da438d44daa7f6b1a88ee4f0aef44863
ssnepenthe <ssnepenthe@users.noreply.github.com>
2016-12-23 11:03:38 -0700

    Assume html context when escaping values in text email template.

--- templates/emails/text-vulnerable.php
+++ templates/emails/text-vulnerable.php
@@ -1,6 +1,11 @@
 <?php
 /**
  * Don't mess with the spacing/indentation!
+ *
+ * Note: Since any site can filter an email's content type before sending, it seems
+ * that we can never be certain that a text email is actually being sent with a text
+ * content type. With that in mind, all values are escaped under the assumption that
+ * this message has been sent with an HTML content type.
  */
 
 ?>Vulnerabilities were detected on <?php echo esc_html( $site_name ) ?>. We've included some details to help you fix the problem.
@@ -15,18 +20,23 @@ A recent scan by the Soter security check plugin flagged <?php echo esc_html( $c
 
 Please ensure your WordPress install as well as all plugins and themes are up-to-date from your dashboard:
 
-Go To Dashboard ( <?php echo $action_url ?> )
+Go To Dashboard ( <?php echo esc_url( $action_url ) ?> )
 
 For reference, here are the details of the flagged vulnerabilities:
 
 <?php foreach ( $messages as $message ) : ?>
-<?php echo $message['title'] ?>
+<?php // This feels so wrong... ?>
+<?php echo esc_html( str_replace(
+	[ '<', '>', '=' ],
+	[ 'less than', 'greater than', ' or equal to' ],
+	$message['title']
+) ) ?>
 
 <?php foreach ( $message['links'] as $url => $host ) : ?>
-<?php echo $url ?>
+<?php echo esc_url( $url ) ?>
 
 <?php endforeach ?>
-<?php echo implode( ' | ', $message['meta'] ) ?>
+<?php echo implode( ' | ', array_map( 'esc_html', $message['meta'] ) ) ?>
 
 
 <?php endforeach ?>
