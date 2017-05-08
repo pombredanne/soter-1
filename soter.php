a25165e9ab697071c05881247c9a54a4a63eef13
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-05-08 12:13:59 -0700

    Move plugin init to dedication function

--- soter.php
+++ soter.php
@@ -35,39 +35,40 @@ function _soter_require_if_exists( $file ) {
 	}
 }
 
-_soter_require_if_exists( __DIR__ . '/vendor/autoload.php' );
+function _soter_init() {
+	$checker = new WP_Requirements\Plugin_Checker( 'Soter', __FILE__ );
 
-$soter_checker = new WP_Requirements\Plugin_Checker( 'Soter', __FILE__ );
+	// Short array syntax.
+	$checker->php_at_least( '5.4' );
 
-// Variadic functions, argument unpacking, use function.
-$soter_checker->php_at_least( '5.6' );
+	if ( $checker->requirements_met() ) {
+		$plugin = new Soter\Plugin( [
+			'dir' => plugin_dir_path( __FILE__ ),
+			'file' => __FILE__,
+			'prefix' => 'soter',
+			'url' => 'https://github.com/ssnepenthe/soter',
+			'version' => '0.4.0',
+		] );
 
-if ( $soter_checker->requirements_met() ) {
-	$soter_plugin = new Soter\Plugin( [
-		'dir' => plugin_dir_path( __FILE__ ),
-		'file' => __FILE__,
-		'prefix' => 'soter',
-		'url' => 'https://github.com/ssnepenthe/soter',
-		'version' => '0.4.0',
-	] );
+		$providers = [
+			new Soter\Listeners\Listeners_Provider,
+			new Soter\Notices\Notices_Provider,
+			new Soter\Options\Options_Provider,
+			new Soter\Tasks\Tasks_Provider,
+			new Soter\Views\View_Provider,
+			new Soter\Plugin_Provider,
+			new Soter\Soter_Core_Provider,
+		];
 
-	$providers = [
-		new Soter\Listeners\Listeners_Provider,
-		new Soter\Notices\Notices_Provider,
-		new Soter\Options\Options_Provider,
-		new Soter\Tasks\Tasks_Provider,
-		new Soter\Views\View_Provider,
-		new Soter\Plugin_Provider,
-		new Soter\Soter_Core_Provider,
-	];
+		foreach ( $providers as $provider ) {
+			$plugin->register( $provider );
+		}
 
-	foreach ( $providers as $provider ) {
-		$soter_plugin->register( $provider );
+		add_action( 'plugins_loaded', [ $plugin, 'boot' ] );
+	} else {
+		$checker->deactivate_and_notify();
 	}
-
-	add_action( 'plugins_loaded', [ $soter_plugin, 'boot' ] );
-} else {
-	$soter_checker->deactivate_and_notify();
 }
 
-unset( $soter_checker, $soter_plugin );
+_soter_require_if_exists( __DIR__ . '/vendor/autoload.php' );
+_soter_init();
