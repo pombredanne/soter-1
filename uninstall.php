12811903f44a52786e5658a21905c6eb310f2b70
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-05-24 13:56:26 -0700

    Load autoloader in uninstall script

--- uninstall.php
+++ uninstall.php
@@ -10,6 +10,24 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 }
 
 /**
+ * Require a file (once) if it exists.
+ *
+ * Needed because the main plugin file isn't loaded on uninstall. Wrapped in a
+ * conditional check just to be safe.
+ *
+ * @param  string $file The file to check and require.
+ *
+ * @return void
+ */
+if ( ! function_exists( '_soter_require_if_exists' ) ) {
+	function _soter_require_if_exists( $file ) {
+		if ( file_exists( $file ) ) {
+			require_once $file;
+		}
+	}
+}
+
+/**
  * The plugin uninstaller.
  *
  * @return void
@@ -32,4 +50,5 @@ function _soter_uninstall() {
 	( new Soter_Core\WP_Transient_Cache( $GLOBALS['wpdb'], 'soter' ) )->flush();
 }
 
+_soter_require_if_exists( __DIR__ . '/vendor/autoload.php' );
 _soter_uninstall();
